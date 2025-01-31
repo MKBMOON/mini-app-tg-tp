6a349ee03aabf44a183e4f56f8bed634d93b0dee
Danon mkb.moon <mkb.moon@yandex.ru>
2025-01-31 13:40:11 +0300

    new url

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 
 TOKEN = '7813747320:AAG7DPyKDs7fivysMVxcMo4TX7TNu1lnR20'
 CHAT_ID = '356523865'
-URL = 'https://your-mini-app.vercel.app'
+URL = 'https://amvera-mkbmoon-run-takeoffmai-miniapp'
 
 message = f'Нажмите на ссылку, чтобы открыть Mini App: {URL}'
 url = f'https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={CHAT_ID}&text={message}'
